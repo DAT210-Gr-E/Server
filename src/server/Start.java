5ce34d1e0e5cafca5f49f448104395b67bb9ae07
mindejulian <mindejulian@me.com>
2013-10-14 12:19:32 +0200

    Vekk fra static

---
+++ src/server/Start.java
@@ -0,0 +1,14 @@
+package server;
+
+public class Start {
+	public void Start() 
+	{
+
+	//Start getter i egen trŒd
+	
+	
+	// Lytt etter TCP connections
+	// Start connections i egne trŒder. type listener.
+	
+	}
+}
