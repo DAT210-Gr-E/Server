c51dd4e2e072e06a868d6a09ba701a56d0fc58ce
janickef <janicke91@gmail.com>
2013-10-16 12:04:34 +0200

    mte

---
+++ src/server/Listener.java
@@ -0,0 +1,22 @@
+package server;
+
+import interfaces.IListener;
+
+public class Listener implements IListener {
+
+	@Override
+	public Picture[] getLinks(String[] hashtag) {
+		// TODO Auto-generated method stub
+		//HEr må morten legge inn database magien sin
+		System.out.println();
+		return null;
+		
+	}
+
+	@Override
+	public boolean checkURL(Picture[] img) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
