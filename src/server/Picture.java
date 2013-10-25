5c1179cba426c0f821e0ab8d01282ad7b5af14d2
janickef <janicke91@gmail.com>
2013-10-25 16:42:40 +0200

    Fiksing i Picture-klassen

--- src/server/Picture.java
+++ src/server/Picture.java
@@ -7,31 +7,19 @@ public class Picture {
     public String description;
     public String type;
 	
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
 	public String getStandardURL() {
 		return standardURL;
-	}
-	public void setStandardURL(String standardURL) {
-		this.standardURL = standardURL;
 	}
 	public String getThumbnailURL() {
 		return thumbnailURL;
 	}
-	public void setThumbnailURL(String lowThumbnailURL) {
-		this.thumbnailURL = lowThumbnailURL;
-	}
 	public String[] getTags() {
 		return tags;
 	}
-	public void setTags(String[] tags) {
-		this.tags = tags;
-	}
 	public String getDescription() {
 		return description;
+	}
+	public String getType() {
+		return type;
 	}
 }
