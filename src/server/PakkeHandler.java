a93757fa965f055bcfcf0e2b1388547fab126016
mindejulian <mindejulian@me.com>
2013-11-07 20:00:38 +0100

    Endret pakkeHandler til å håndtere den nye Pakke klassen

--- src/server/PakkeHandler.java
+++ src/server/PakkeHandler.java
@@ -1,6 +1,7 @@
 package server;
 
 import klient.Nettverk.Pakke;
+import klient.Nettverk.Pakke.TransaksjonsType;
 
 public class PakkeHandler {
 	public PakkeHandler() {
@@ -11,69 +12,70 @@ public class PakkeHandler {
 		Pakke utPakken = null;
 		int transID = pakke.getTransaksjonsid();
 		
-		if(pakke.getPakkeType() == Pakke.PakkeType.FORESPOERSEL_OM_DEFAULT_URL_LISTE_OG_TID_FRA_SERVER){
-			//Hent fra database. Morten hva får jeg her?
-			//Enten kan jeg ta imot en pakke eller string[]urlliste og string[]tags
-			//Kun for test:
-			String[] u = {"http://www.wallng.com/images/2013/08/image-explosion-colors-background-beautiful-263613.jpg",
-							"file:///C:/Users/Bruker/desktop/2012-12-10_03.12.03.png",
-							"http://www.nasa.gov/images/content/693952main_pia15817-full_full.jpg"};
-			String[] t = {"aaa", "bbb", "ccc"};
-		
-			utPakken = new Pakke(transID, u, t);
+		if(pakke.getPakkeType() == Pakke.PakkeType.SPOER_OM_TID){
+			int tid = 5;
+			//Tid hentes fra databasen.
+			utPakken = new Pakke(transID, TransaksjonsType.TID, tid);
 		}
-		else if(pakke.getPakkeType() == Pakke.PakkeType.SET_DEFAULT_TID){
-			int tid = pakke.getTid();
-			//tid må legges inn i databasen.
+		else if(pakke.getPakkeType() == Pakke.PakkeType.SPOER_OM_BILDER){
+			String[] bilder = {	"http://www.wallng.com/images/2013/08/image-explosion-colors-background-beautiful-263613.jpg",
+								"file:///C:/Users/Bruker/desktop/2012-12-10_03.12.03.png",
+								"http://www.nasa.gov/images/content/693952main_pia15817-full_full.jpg"
+								};
+			//Bilder hentes fra databasen.
 			
-			
-			utPakken = new Pakke(transID, tid);
+			utPakken = new Pakke(transID, TransaksjonsType.BILDER, bilder);
 		}
-		else if(pakke.getPakkeType() == Pakke.PakkeType.TAGS_LISTE_TIL_SERVER){
-			String[] tags = pakke.getTags();
-			//tags til database...
-			//Hent fra database. Morten hva får jeg her?
-			//Enten kan jeg ta imot en pakke eller string[]urlliste og string[]tags
-			//Kun for test:
-			String[] u = {"http://www.wallng.com/images/2013/08/image-explosion-colors-background-beautiful-263613.jpg",
-							"file:///C:/Users/Bruker/desktop/2012-12-10_03.12.03.png",
-							"http://www.nasa.gov/images/content/693952main_pia15817-full_full.jpg"};
-			String[] t = {"aaa", "bbb", "ccc"};
-		
-			utPakken = new Pakke(transID, u, t);
-		}
-		else if( pakke.getPakkeType() == Pakke.PakkeType.URL_LISTE_OG_TAGS_ECHO_FRA_SERVER){
-			utPakken = null; //evt error?
-		}
-		else if( pakke.getPakkeType() == Pakke.PakkeType.LOGIN_PASSORD_TIL_SERVER){
+		else if(pakke.getPakkeType() == Pakke.PakkeType.SPOER_OM_LOGIN){
 			String passord = pakke.getPassord();
-			//Sjekk med databasen om pasordet er rikgtig.
-			//foreløpig: 
-			if (passord == "hemmelig") {
-				utPakken = new Pakke(transID, true);
-			} 
-			else {
-				utPakken = new Pakke(transID, false);
+			boolean success;
+			if(passord == "aaaa")
+			{
+				success = true;
 			}
+			else 
+			{
+				success = false;
+			}
+		
+			utPakken = new Pakke(transID, TransaksjonsType.LOGIN, passord, success);
 		}
-		else if(pakke.getPakkeType() == Pakke.PakkeType.LOGIN_SVAR_FRA_SERVER){
-			utPakken = null; //evt error?
+		else if( pakke.getPakkeType() == Pakke.PakkeType.SPOER_OM_TAGS){
+			String[] tags = {"aaa", "bbb", "ccc" };
+			//tags hentes fra databasen.
+			
+			utPakken = new Pakke(transID, TransaksjonsType.TAGS, tags);
 		}
-		else if( pakke.getPakkeType() == Pakke.PakkeType.ADMIN_TAGS_LISTE_TIL_SERVER){
-			//antar denne er for å legge til nye tags.
+		else if( pakke.getPakkeType() == Pakke.PakkeType.ADMIN_SPOER_OM_BILDER){
+			String[] alleBilder = {	"http://www.wallng.com/images/2013/08/image-explosion-colors-background-beautiful-263613.jpg",
+									"file:///C:/Users/Bruker/desktop/2012-12-10_03.12.03.png",
+									"http://www.nasa.gov/images/content/693952main_pia15817-full_full.jpg"
+									};
+			boolean[] blokkert = {false, true, false};
+			
+				utPakken = new Pakke(transID, TransaksjonsType.ADMIN_BILDER, alleBilder, blokkert);
+		}
+		else if(pakke.getPakkeType() == Pakke.PakkeType.ADMIN_SET_DEFAULT_TAGS){
 			String[] tags = pakke.getTags();
-			//tags må legges til databasen
-			utPakken = new Pakke(transID, tags, true);
+			//tags legges så inn i databasen.
+			
+			
+			utPakken = new Pakke(transID, TransaksjonsType.TAGS, tags);
 		}
-		else if( pakke.getPakkeType() == Pakke.PakkeType.ADMIN_SET_DEFAULT_TAGSLISTE){
-			//er dette da for å overskrive hele lista?
-			utPakken = null; //evt error?
+		else if( pakke.getPakkeType() == Pakke.PakkeType.ADMIN_SET_DEFAULT_TID){
+			int tid = pakke.getTid();
+			
+			utPakken = new Pakke(transID, TransaksjonsType.TID, tid);
 		}
-		else if( pakke.getPakkeType() == Pakke.PakkeType.ADMIN_INKLUDER_URL_LISTE){
-			utPakken = null; //evt error?
+		else if( pakke.getPakkeType() == Pakke.PakkeType.ADMIN_SET_SVARTELISTE){
+			String[] bilder = pakke.getUrls();
+			boolean[] inkluderte = pakke.getInkluderte();
+			
+			utPakken = new Pakke(transID, TransaksjonsType.ADMIN_BILDER, bilder, inkluderte);
 		}
-		else if( pakke.getPakkeType() == Pakke.PakkeType.ADMIN_EKSKLUDER_URL_LISTE){
-			utPakken = null; //evt error?
+		else
+		{
+			//UGYLDIG_PAKKE
 		}
 		
 		
