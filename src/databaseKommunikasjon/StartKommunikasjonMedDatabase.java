e1c33db04eb349ac2625b9ddb0b3fe52cbe9c260
Morten Nordbø <m.nordbo@stud.uis.no>
2013-12-05 13:47:20 +0100

    Ferdig?

--- src/databaseKommunikasjon/StartKommunikasjonMedDatabase.java
+++ src/databaseKommunikasjon/StartKommunikasjonMedDatabase.java
@@ -13,34 +13,8 @@ public class StartKommunikasjonMedDatabase
 
 	public StartKommunikasjonMedDatabase()
 	{
-
-		
-		Statement setning;
-		ResultSet resultat;
-		String sporring;
-		String title;
-		
-
-		try {
-			Connection kobling = DriverManager.getConnection("jdbc:mysql://168.61.96.122:7000/test", "user", "root" );
-		{
-			sporring = "select * from example_autoincrement";
-			setning = kobling.createStatement();
-			resultat = setning.executeQuery(sporring);
-			while(resultat.next())
-			{
-				title = resultat.getString(1);
-				System.out.println(title);
-			}
-
-		}
-		
-		} catch (SQLException e) {
-			
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		DatabaseMetoder db = new DatabaseMetoder();
+		db.RestartDataBaseLinks();
 	}
 	
 }
